114dbde428b796780e462e900e5343931168a74c
jhkim0228@tangunsoft.com <jhkim0228@tangunsoft.com>
2023-02-22 16:17:53 +0900

    error_file commit

---
+++ TodoApp-NoChain-KTS-master/src/main/java/hello/Task.java
@@ -0,0 +1,26 @@
+package hello;
+
+import org.springframework.data.annotation.Id;
+
+public class Task {
+
+	@Id private String id;
+
+	private Striscription;
+	private Bocked;
+
+	public Boolean get {
+		return checked;
+	}
+
+	public void setChecked(Bocked;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		tcription = description;
+	}
+}
